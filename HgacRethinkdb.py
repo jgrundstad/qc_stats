e76a1cb4c77c3567ac70164628cd387521ce5475
Jason Grundstad <jgrundstad@gmail.com>
2014-09-29 10:23:22 -0500

    connects with internal methods, query col pairs

--- HgacRethinkdb.py
+++ HgacRethinkdb.py
@@ -1,53 +1,58 @@
 import rethinkdb as r
 import sys
-import argparse
-from StringIO import StringIO
 
 class HgacRethinkdb:
-  
+  '''
+  Provide functionality for querying the HGAC metadata 
+  stored in rethinkDB
+  __init__ creates the connection
+  get_pair_columns returns dict of 2 queried columns
+  '''
 
   def __init__(self, host, db, keyfile):
+    '''
+    Read keyfile and create connection
+    '''
     self._host = host
     self._db = db
     self._keyfile = keyfile
-    self._auth_key = read_keyfile(self._keyfile)
-    self._conn = connect(host, self._auth_key)
+    self._auth_key = self.__read_keyfile(self._keyfile)
+    self._conn = self.__connect(host, self._auth_key)
 
 
-  def read_keyfile(self, keyfile):
-    fh = open(keyfile, 'r')
-    AUTH_KEY = fh.readline().strip()
-    fh.close()
-    return AUTH_KEY
+  def __read_keyfile(self, keyfile):
+    try:
+      fh = open(keyfile, 'r')
+      AUTH_KEY = fh.readline().strip()
+      fh.close()
+      return AUTH_KEY
+    except:
+      print >>sys.stderr, "[HgacRethinkdb exception] - Unable to parse keyfile: " + keyfile
+      sys.exit(1)
 
-  def connect(self, host, AUTH_KEY):
-    return r.connect(host, auth_key=AUTH_KEY)
 
-  #def print_columns(db, table, conn, cols):
-  #  for record in r.db(db).table(table).pluck(cols).run(conn):
-  #    print('\t'.join([str(x) for x in record.values()]))
+  def __connect(self, host, AUTH_KEY):
+    try: 
+      return r.connect(host, auth_key=AUTH_KEY)
+    except:
+      print >>sys.stderr, "[HgacRethinkdb excpetion] - Unable to connect to host: " + host
+      sys.exit(1)
+
 
   def print_columns(self, table, cols):
     for record in r.db(self._db).table(table).pluck(cols).run(self._conn):
       print('\t'.join([str(x) for x in record.values()]))
 
-'''
-def main():
 
-  parser = argparse.ArgumentParser(description='query FO_NBCP Library Tracker Google Doc')
-  parser.add_argument('--keyfile', '-k', dest='keyfile', action='store', required=True, help='Auth_key file')
-  args = parser.parse_args()
-  
-  AUTH_KEY = read_keyfile(args.keyfile)
-  db = 'hgac'
-  table = 'FONBC_LibraryTracker'
-  file_column = 'Total # of Sequence Files from PE runs (AD)'
+  def get_pair_columns(self, table, cols):
+    d = dict()
+    for record in r.db(self._db).table(table).pluck(cols).run(self._conn):
+      if record.values()[1]:
+        d[record.values()[0]] = record.values()[1]
+      else:
+        d[record.values()[0]] = '-'
 
-  conn = connect('igsbimg.uchicago.edu', AUTH_KEY)
+    return d
 
-  print_columns(db, table, conn, ['BID', file_column])
- 
 
-if __name__ == '__main__':
-  main()
-'''
+
